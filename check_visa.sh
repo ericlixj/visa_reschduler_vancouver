047e7c9ec789faf69240d5984cc8bc628def2a15
EricLi <ericli82ca@gmail.com>
2025-08-13 16:51:08 -0700

    update

--- check_visa.sh
+++ check_visa.sh
@@ -8,8 +8,8 @@ PROCESS_NAME="visa.py"
 
 # 检查进程是否存在
 if ! pgrep -f "$PROCESS_NAME" > /dev/null; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $PROCESS_NAME not running, starting..." >> /tmp/visa_cron.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $PROCESS_NAME not running, starting..." >> ./visa.log
     /usr/bin/python3 visa.py >> /tmp/visa_cron.log 2>&1 &
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $PROCESS_NAME is running." >> /tmp/visa_cron.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $PROCESS_NAME is running." >> ./visa.log
 fi
